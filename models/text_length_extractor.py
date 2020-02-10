b1cf8ccd05600b8f9b3063f7e8237d2ee2427a85
Olivier Gobron <gobron.olivier@gmail.com>
2020-02-10 16:46:10 +0100

    pep8 modifications

--- models/text_length_extractor.py
+++ models/text_length_extractor.py
@@ -6,15 +6,25 @@ import pandas as pd
 
 
 class TextLengthExtractor(BaseEstimator, TransformerMixin):
+    """ Class of the text length extractor used in the classifier model."""
 
-    def get_length(self, text):
+    @staticmethod
+    def get_length(text):
+        """ Extract the length of a text.
+        
+            Args :
+                text (str) : text
+               
+           Output : 
+                (int) : number of words in the text
+          """
         words = nltk.word_tokenize(text)
         words = [w for w in words if w not in stopwords.words("english")]
         return len(words)
 
-    def fit(self, X, y=None):
+    def fit(self, x, y=None):
         return self
 
-    def transform(self, X):
-        X_tagged = pd.Series(X).apply(self.get_length)
-        return pd.DataFrame(X_tagged)
+    def transform(self, x):
+        x_tagged = pd.Series(x).apply(self.get_length)
+        return pd.DataFrame(x_tagged)
